3478abf6f90dfd2e6b3322e63baae00568f46364
Miles Crighton <miles.crighton@gmail.com>
2020-04-02 22:10:08 +0100

    Removed old resolver

--- src/index.js
+++ src/index.js
@@ -6,48 +6,6 @@ const resolvers = require("./resolvers");
 
 const Covid19API = require("./datasources/covid19");
 
-// const resolvers = {
-//     Summary: {
-//         Country: parent => {
-//             return parent.Country;
-//         },
-//         CountrySlug: parent => {
-//             return parent.Slug;
-//         },
-//         NewConfirmed: parent => {
-//             return parent.NewConfirmed;
-//         },
-//         TotalConfirmed: parent => {
-//             return parent.TotalConfirmed;
-//         },
-//         NewDeaths: parent => {
-//             return parent.NewDeaths;
-//         },
-//         TotalDeaths: parent => {
-//             return parent.TotalDeaths;
-//         },
-//         NewRecovered: parent => {
-//             return parent.TotalRecovered;
-//         },
-//         TotalRecovered: parent => {
-//             return parent.TotalRecovered;
-//         }
-//     },
-//     Query: {
-//         summary: async (_, { countries }) => {
-//             const res = await axios.get(
-//                 "https://api.covid19api.com/summary",
-//                 {}
-//             );
-//             let countryData = res.data.Countries;
-//             countryData = countryData.filter(countrySummary => {
-//                 return countries.includes(countrySummary.Country);
-//             });
-//             return countryData;
-//         }
-//     }
-// };
-
 const server = new ApolloServer({
     typeDefs,
     resolvers,
