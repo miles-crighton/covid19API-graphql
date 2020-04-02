63d92418b52d3adfbd6d8f90f2e3434b7a1ad7df
Miles Crighton <miles.crighton@gmail.com>
2020-04-02 19:18:27 +0100

    Working summary data per country resolvers

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
 const express = require("express");
+const axios = require("axios");
 const { ApolloServer, gql } = require("apollo-server-express");
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
     type Query {
-        hello: String
+        summary(countries: [String]): [Summary]
+    }
+
+    type Summary {
+        Country: String
+        CountrySlug: String
+        NewConfirmed: Int
+        TotalConfirmed: Int
+        NewDeaths: Int
+        TotalDeaths: Int
+        NewRecovered: Int
+        TotalRecovered: Int
     }
 `;
 
 // Provide resolver functions for your schema fields
 const resolvers = {
+    Summary: {
+        Country: parent => {
+            return parent.Country;
+        },
+        CountrySlug: parent => {
+            return parent.Slug;
+        },
+        NewConfirmed: parent => {
+            return parent.NewConfirmed;
+        },
+        TotalConfirmed: parent => {
+            return parent.TotalConfirmed;
+        },
+        NewDeaths: parent => {
+            return parent.NewDeaths;
+        },
+        TotalDeaths: parent => {
+            return parent.TotalDeaths;
+        },
+        NewRecovered: parent => {
+            return parent.TotalRecovered;
+        },
+        TotalRecovered: parent => {
+            return parent.TotalRecovered;
+        }
+    },
     Query: {
-        hello: () => "Hello world!"
+        summary: async (_, { countries }) => {
+            const res = await axios.get(
+                "https://api.covid19api.com/summary",
+                {}
+            );
+            console.log(countries);
+            let countryData = res.data.Countries;
+            countryData = countryData.filter(countrySummary => {
+                return countries.includes(countrySummary.Country);
+            });
+            return countryData;
+        }
     }
 };
 
